f98fe1526c921a9e82190cfd6e4e556b6c0f0e95
Mathias Jacquelin <mjacquelin@lbl.gov>
2016-07-14 14:42:26 -0700

    adding new openmp gemm routine

---
+++ LagrangeMultiplier2-Reduce/gemm.c
@@ -0,0 +1,99 @@
+#include <stdio.h>      /* Standard Library of Input and Output */
+#include <complex.h>    /* Standard Library of Complex Numbers */
+#include <math.h>  
+#include<omp.h>
+
+ #define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+double gemmOMPThreshold = 64^3;
+
+
+
+void zgemm_omp_task(char transa, char transb, int m, int n, int k, double complex alpha, double complex* a, int lda, double complex* b, int ldb, double complex beta, double complex* c, int ldc, int depth) {
+//double task_recursion_cutoff_level = (log(omp_get_num_threads())/log(2) + 3 ) ;
+double task_recursion_cutoff_level = (log2(omp_get_num_threads()) + 3 ) ;
+	if (depth>=task_recursion_cutoff_level || (double)m*n*k <= gemmOMPThreshold)
+		zgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc);
+	else {
+		char opA = transa=='T'||transa=='t'||transa=='C'||transa=='c';
+		char opB = transb=='T'||transb=='t'||transb=='C'||transb=='c';
+		if (n >= max(m,k)) {
+#pragma omp task final(depth >= task_recursion_cutoff_level-1) mergeable
+			zgemm_omp_task(transa, transb, m, n/2, k, alpha, a, lda, b, ldb, beta, c, ldc, depth+1);                                                            // C0 = alpha * A*B0 + beta * C0
+#pragma omp task final(depth >= task_recursion_cutoff_level-1) mergeable
+			zgemm_omp_task(transa, transb, m, n-n/2, k, alpha, a, lda, opB ? b+n/2 : b+(n/2)*ldb, ldb, beta, c+(n/2)*ldc, ldc, depth+1);                        // C1 = alpha * A*B1 + beta * C1
+#pragma omp taskwait
+		} else if (m >= k) {
+#pragma omp task final(depth >= task_recursion_cutoff_level-1) mergeable
+			zgemm_omp_task(transa, transb, m/2, n, k, alpha, a, lda, b, ldb, beta, c, ldc, depth+1);                                                            // C0 = alpha * A0*B + beta * C0
+#pragma omp task final(depth >= task_recursion_cutoff_level-1) mergeable
+			zgemm_omp_task(transa, transb, m-m/2, n, k, alpha, opA ? a+(m/2)*lda : a+m/2, lda, b, ldb, beta, c+m/2, ldc, depth+1);                              // C1 = alpha * A1*B + beta * C1
+#pragma omp taskwait
+		} else {
+			zgemm_omp_task(transa, transb, m, n, k/2, alpha, a, lda, b, ldb, beta, c, ldc, depth);                                                         // C  = alpha * A0*B0 + beta * C
+			zgemm_omp_task(transa, transb, m, n, k-k/2, alpha, opA ? a+k/2 : a+(k/2)*lda, lda, opB ? b+(k/2)*ldb : b+k/2, ldb, (double complex)1., c, ldc, depth); // C += alpha * A1*B1
+		}
+	}
+}
+
+void dgemm_omp_task(char transa, char transb, int m, int n, int k, double alpha, double* a, int lda, double* b, int ldb, double beta, double* c, int ldc, int depth) {
+//double task_recursion_cutoff_level = (log(omp_get_num_threads()) + 3 ) / log(2);
+double task_recursion_cutoff_level = (log2(omp_get_num_threads()) + 3 ) ;
+	if (depth>=task_recursion_cutoff_level || (double)m*n*k <= gemmOMPThreshold)
+	{
+	//printf("actual gemm call\n");
+	//	printf("%d %d %d %lf %x %d %x %d %lf %x %d %d %lf %lf\n", m, n,  k, alpha,  a, lda,  b, ldb,  beta,  c, ldc, depth,task_recursion_cutoff_level,gemmOMPThreshold);
+		dgemm(&transa, &transb, &m, &n, &k, &alpha, a, &lda, b, &ldb, &beta, c, &ldc);
+	//printf("after actual gemm call\n");
+	}
+	else {
+		char opA = transa=='T'||transa=='t'||transa=='C'||transa=='c';
+		char opB = transb=='T'||transb=='t'||transb=='C'||transb=='c';
+		if (n >= max(m,k)) {
+#pragma omp task final(depth >= task_recursion_cutoff_level-1) mergeable
+			dgemm_omp_task(transa, transb, m, n/2, k, alpha, a, lda, b, ldb, beta, c, ldc, depth+1);                                                            // C0 = alpha * A*B0 + beta * C0
+#pragma omp task final(depth >= task_recursion_cutoff_level-1) mergeable
+			dgemm_omp_task(transa, transb, m, n-n/2, k, alpha, a, lda, opB ? b+n/2 : b+(n/2)*ldb, ldb, beta, c+(n/2)*ldc, ldc, depth+1);                        // C1 = alpha * A*B1 + beta * C1
+#pragma omp taskwait
+		} else if (m >= k) {
+#pragma omp task final(depth >= task_recursion_cutoff_level-1) mergeable
+			dgemm_omp_task(transa, transb, m/2, n, k, alpha, a, lda, b, ldb, beta, c, ldc, depth+1);                                                            // C0 = alpha * A0*B + beta * C0
+#pragma omp task final(depth >= task_recursion_cutoff_level-1) mergeable
+			dgemm_omp_task(transa, transb, m-m/2, n, k, alpha, opA ? a+(m/2)*lda : a+m/2, lda, b, ldb, beta, c+m/2, ldc, depth+1);                              // C1 = alpha * A1*B + beta * C1
+#pragma omp taskwait
+		} else {
+			dgemm_omp_task(transa, transb, m, n, k/2, alpha, a, lda, b, ldb, beta, c, ldc, depth);                                                         // C  = alpha * A0*B0 + beta * C
+			dgemm_omp_task(transa, transb, m, n, k-k/2, alpha, opA ? a+k/2 : a+(k/2)*lda, lda, opB ? b+(k/2)*ldb : b+k/2, ldb, (double)1., c, ldc, depth); // C += alpha * A1*B1
+		}
+	}
+}
+
+
+void dgemm_omp_(char * transa, char * transb, int* m, int* n, int* k, double* alpha, double* a, int* lda, double* b, int* ldb, double* beta, double* c, int* ldc) {
+//	int i,j;
+//    printf("%d %d %d %lf %x %d %x %d %lf %x %d\n", *m, *n,  *k, *alpha,  a, *lda,  b, *ldb,  *beta,  c, *ldc);
+//	for(i = 0; i<*m; i++){
+//		for(j = 0; j<*n; j++){
+//			double t = c[i+j*(*ldc)];
+//			c[i+j*(*ldc)] = t;
+//		}
+//	}
+//	for(i = 0; i<*m; i++){
+//		for(j = 0; j<*k; j++){
+//			double t = a[i+j*(*lda)];
+//			a[i+j*(*lda)] = t;
+//		}
+//	}
+//	for(i = 0; i<*k; i++){
+//		for(j = 0; j<*n; j++){
+//			double t = b[i+j*(*ldb)];
+//			b[i+j*(*ldb)] = t;
+//		}
+//	}
+//	dgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc); 
+	#pragma omp single nowait
+	dgemm_omp_task(*transa, *transb, *m, *n, *k, *alpha, a, *lda, b, *ldb, *beta, c, *ldc, 0);                                                         
+}
